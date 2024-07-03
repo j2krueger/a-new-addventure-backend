60f1f3b30e58bdae29e50d26d151a502897025f3
John Krueger <j_krueger@yahoo.com>
2024-07-03 12:39:36 -0400

    ??? Mongo connect error

---
+++ controllers/entry.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// const constants = require('../helpers/constants');
+const Entry = require('../models/entry');
+
+async function paramEntryID(req, res, next, value) {
+  const entryID = value;
+  try {
+      const result = await Entry.findById(entryID);
+      if (result) {
+          req.foundEntryByID = result;
+      }
+      next();
+  } catch (err) {
+      return next(err)
+  }
+}
+
+const getEntry = async(req, res) => {
+    try {
+     const entryId = req.params.id;
+     const entry = await Entry.findById(entryId);
+ 
+     // Send the user data back to the client
+     res.json(entry);
+   } catch (error) {
+     console.error("Error fetching entry:", error);
+     res.status(500).json({ error: "Internal Server Error" });
+   }
+ };
+
+ const createStory = async (req, res) => {
+  try {
+    const {storyTitle, body, authorName } =
+      req.body;
+
+    const createdStoryId = crypto.randomBytes(16).toString("hex");
+
+    const entry = await Entry.create({
+      storyId: createdStoryId,
+      entryTitle: null,
+      storyTitle,
+      bodyText: body,
+      previousEntry: null,
+      authorName
+    });
+    return res.json(entry);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  paramEntryID,
+  getEntry,
+  createStory,
+}
---
+++ models/entry.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const { ObjectId } = require('mongodb');
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const entrySchema = new Schema({
+    storyId: {
+      type: ObjectId,
+      required: ["Needs the 1st entry in this story"],
+    },
+    authorName: {
+      type: String,
+      required: ["Author is needed"],
+    },
+    entryTitle: {
+      type: String,
+      required: ['Entry Title is needed'],
+    },
+    storyTitle: {
+      type: String,
+      required: ['Story Title is needed'],
+    },
+    bodyText: {
+        type: String,
+        required: ['The body is needed'],
+      },
+    previousEntry: {
+        type: ObjectId,
+        required: ['Previous Entry is needed'],
+      },
+    createDate: {
+        type: Date,
+        required: ['Author is needed'],
+      },
+    flagId: {
+      type: ObjectId,
+      default: null,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    }
+  });
+
+
+const Entry = mongoose.model("Entry", entrySchema)
+
+module.exports = Entry
