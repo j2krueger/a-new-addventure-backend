dbd1e9f7aca37d15edab840f6ece58d3111a1bac
John Krueger <j_krueger@yahoo.com>
2024-09-09 17:44:16 -0400

    Improve email messages

--- helpers/mail.js
+++ helpers/mail.js
@@ -12,16 +12,35 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+const commonPreHtml = `<!doctype html>
+<html>
+    <head>
+        <title>
+        </title>
+        <style>
+            .myStyle {
+                font-size: 24px !important;
+            }
+        </style>
+    </head>
+<body><span class="myStyle">`;
+const commonPostHtml = `</span></body></html>`;
+
 
 async function sendVerificationEmailHelper(user) {
     const userId = user._id;
     const email = user.email.includes(constants.testString) ? constants.testEmailAddress : unescapeHTML(user.email);
+    const html = commonPreHtml + `<p>Hello, ${user.userName}, and welcome to QuiltedChronicles.org!</p>
+        <p>To verify your email address at QuiltedChronicles.org, just
+        <a href="https://quiltedchronicles.org/verify/${userId}/${user.emailVerificationKey}">Click here</a>.</p>
+        <p>If you don&apos;t know why you have recieved this email, you can safely mark it as spam, delete it, or ignore it.
+        If you recieve several of these emails and are unhappy about it, you can contact us at ${constants.siteEmailAddress},
+        and we&apos;ll fix the issue as quickly as we can.</p>`+ commonPostHtml;
     transporter.sendMail({
         from: constants.siteEmailAddress,
         to: email,
         subject: "Email Verification from QuiltedChronicles.org",
-        html: `Just a basic link: <a href="https://quiltedchronicles.org/verify/${userId}/${user.emailVerificationKey}">Click here to verify</a>
-        UserName: ${user.userName}, email: ${user.email}`,
+        html,
     }).catch((error) => {
         console.log(Date.now(), ': transporter.sendMail failed in sendVerificationEmailHelper: ', error);
     });
@@ -30,12 +49,17 @@ async function sendVerificationEmailHelper(user) {
 async function sendResetPasswordEmailHelper(user) {
     const userId = user._id;
     const email = user.email.includes(constants.testString) ? constants.testEmailAddress : unescapeHTML(user.email);
+    const html = commonPreHtml + `<p>Hello, ${user.userName}, we&apos;ve recieved your request to reset your password. Just
+        <a href="https://quiltedchronicles.org/resetpassword/${userId}/${user.resetPasswordKey}">click here</a> and follow the
+        instructions on the page it takes you to.</p>
+        <p>If you don&apos;t know why you have recieved this email, you can safely mark it as spam, delete it, or ignore it.
+        If you recieve several of these emails and are unhappy about it, you can contact us at ${constants.siteEmailAddress},
+        and we&apos;ll fix the issue as quickly as we can.</p>`+ commonPostHtml;
     transporter.sendMail({
         from: constants.siteEmailAddress,
         to: email,
         subject: "Password Reset from QuiltedChronicles.org",
-        html: `Just a basic link: <a href="https://quiltedchronicles.org/resetpassword/${userId}/${user.resetPasswordKey}">Click here to reset your password</a>
-        UserName: ${user.userName}, email: ${user.email}`,
+        html,
     }).catch((error) => {
         console.log(Date.now(), ': transporter.sendMail failed in sendResetPasswordEmailHelper: ', error);
     });
